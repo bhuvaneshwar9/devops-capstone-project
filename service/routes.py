f7ba080c19712a1f40d54282dfffb3319579e35d
bhuvaneshwar9 <bhuvaneshwarreddy7@gmail.com>
2025-04-02 14:44:16 -0400

    Implemented delete account functionality

--- service/routes.py
+++ service/routes.py
@@ -158,3 +158,21 @@ def test_update_account_not_found(self):
     test_account = AccountFactory()
     resp = self.client.put(f"{BASE_URL}/0", json=test_account.serialize())
     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+######################################################################
+# DELETE AN ACCOUNT
+######################################################################
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_account(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        
+    account.delete()
+    
+    return "", status.HTTP_204_NO_CONTENT
