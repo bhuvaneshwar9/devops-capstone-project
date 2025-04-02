7f47575d1a80e6f546f4f11a31c5357c060f2ec4
bhuvaneshwar9 <bhuvaneshwarreddy7@gmail.com>
2025-04-02 14:03:02 -0400

    Implemented list accounts with tests, achieving 96% code coverage

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -8,6 +8,7 @@ Test cases can be run with the following:
 import os
 import logging
 from unittest import TestCase
+from unittest.mock import patch
 from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
@@ -123,4 +124,57 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_list_accounts(self):
+        """It should Get a list of Accounts"""
+        # Create a bunch of accounts
+        self._create_accounts(5)
+        
+        # Get the list back
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        
+        # Check the data that was returned
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
+    
+    def test_list_empty_accounts(self):
+        """It should return an empty list when no accounts exist"""
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 0)
+        
+    def test_method_not_allowed(self):
+        """It should not allow a method that's not allowed"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    def test_bad_request_json(self):
+        """It should return Bad Request for invalid JSON format"""
+        resp = self.client.post(
+            BASE_URL, 
+            data="this is not valid json",
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
+        
+    # Additional test to trigger HTTP_404_NOT_FOUND handler
+    def test_get_account_not_found(self):
+        """It should return 404 for an account that doesn't exist"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
+    # Test to trigger Method Not Allowed (405) error handler
+    def test_method_not_allowed_put(self):
+        """It should not allow PUT on the collection URL"""
+        resp = self.client.put(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+    
+    # Test to force an error that isn't explicitly handled 
+    def test_internal_server_error(self):
+        """It should trigger an internal server error"""
+        # Mock Account.all to raise a generic exception
+        with patch('service.models.Account.all') as mock_all:
+            mock_all.side_effect = Exception("Internal Server Error")
+            resp = self.client.get(BASE_URL)
+            self.assertEqual(resp.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
